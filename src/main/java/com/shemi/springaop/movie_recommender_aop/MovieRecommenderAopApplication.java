91f8d08cbe5d5eaca169c42dcaa7db6ed4817127
Victor Shemi <victor.s@nathandigital.com>
2024-11-24 01:09:23 +0300

    initial commit

---
+++ src/main/java/com/shemi/springaop/movie_recommender_aop/MovieRecommenderAopApplication.java
@@ -0,0 +1,13 @@
+package com.shemi.springaop.movie_recommender_aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommenderAopApplication.class, args);
+	}
+
+}
